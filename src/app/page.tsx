3e37916377f28f4307d755de4dfcf80afcb4a1d6
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2024-06-03 14:54:50 +0300

    test  font-size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import WhyUs from "@/components/WhyUs";
 import s from "@/sass/layouts/home.module.scss";
 
 export default function Home() {
   return (
     <section>
-      <div className={`${s.container}  `}>home</div>
+      <div className={`${s.container}  `}>
+        <WhyUs />
+      </div>
     </section>
   );
 }
